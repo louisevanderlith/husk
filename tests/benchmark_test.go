40cb727470b206131f555b82c02b76f9f5d39ad5
Louise van der Lith <louisevanderlith@gmail.com>
2020-07-06 12:48:08 +0200

    Tape Writer makes better use of Seek

--- tests/benchmark_test.go
+++ tests/benchmark_test.go
@@ -12,7 +12,19 @@ var (
 
 func init() {
 	benchCtx = sample.NewContext()
-	benchCtx.Seed()
+
+}
+
+func BenchmarkSeed_PeoplePopulate(b *testing.B) {
+	DestroyData()
+	err := benchCtx.People.Seed("people.seed.json")
+
+	if err != nil {
+		b.Fatal(err)
+		return
+	}
+
+	benchCtx.People.Save()
 }
 
 func BenchmarkFilter_PerfectBalance(b *testing.B) {
@@ -33,5 +45,15 @@ func BenchmarkFilter_PerfectBalance(b *testing.B) {
 
 	if set.Count() != 10 {
 		b.Errorf("%+v\n", set.Count())
+	}
+}
+
+func BenchmarkCalc_CalculateLowestBalance(b *testing.B) {
+	name := ""
+
+	ctx.People.Calculate(&name, sample.LowestBalance())
+
+	if name != "Kelley" {
+		b.Errorf("name Kelley not found, got %s", name)
 	}
 }
